b720745ae7898695d12f310378ce87e77c947133
Suyash Shendre <67360905+SuyashShendre@users.noreply.github.com>
2023-05-30 13:26:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1687,9 +1687,6 @@ function App() {
                     alt="about"
                   />
             </a>
-            <p class="mt-2 text-sm text-gray-500">
-              Air plant banjo lyft occupy retro adaptogen indego
-            </p>
           </div>
           <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left ">
             
